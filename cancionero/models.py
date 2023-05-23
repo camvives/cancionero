0a5ce3df844bd21981b85f6afad501b9ee2974d1
Camvives <camvives@gmail.com>
2023-05-23 17:20:32 -0300

    Init cancionero

---
+++ cancionero/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class Cancion(models.Model):
+    TONOS = (
+        ("C", "C"),
+        ("C#", "C#"),
+        ("D", "D"),
+        ("D#", "D#"),
+        ("E", "E"),
+        ("F", "F"),
+        ("F#", "F#"),
+        ("G", "G"),
+        ("G#", "G#"),
+        ("A", "A"),
+        ("A#", "A#"),
+        ("B", "B")
+    )
+
+    titulo = models.CharField(max_length=200)
+    tono = models.CharField(
+        max_length = 2,
+        choices = TONOS,
+        default = "C" 
+        )
+    letra = models.TextField()
+
+    def __str__(self):
+        return self.titulo
