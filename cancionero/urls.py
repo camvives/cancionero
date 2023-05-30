8ea27634001ed99cf8ddb0c8cbedfe13e7da78b0
Camvives <camvives@gmail.com>
2023-05-30 17:13:02 -0300

    Detalle cancion

--- cancionero/urls.py
+++ cancionero/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_canciones, name='lista_canciones')
+    path('', views.lista_canciones, name='lista_canciones'),
+    path('cancion/<int:pk>/', views.cancion_detalle, name='cancion_detalle')
 ]
--- cancionero/views.py
+++ cancionero/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cancion
 
 # Create your views here.
 def lista_canciones(request):
     canciones = Cancion.objects.all()
     return render(request, 'cancionero/canciones_lista.html', {'canciones': canciones})
+
+def cancion_detalle(request, pk):
+    cancion = get_object_or_404(Cancion, pk=pk)
+    return render(request, 'cancionero/cancion_detalle.html', {'cancion': cancion})
