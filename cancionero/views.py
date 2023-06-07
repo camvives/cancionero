dfe423bfe72c7829d46eb7735c9b76f076b3dfa5
Camvives <camvives@gmail.com>
2023-06-07 17:12:54 -0300

    autenticación para cambio de tono

--- cancionero/views.py
+++ cancionero/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Cancion
 
 # Create your views here.
@@ -10,7 +11,7 @@ def cancion_detalle(request, pk):
     cancion = get_object_or_404(Cancion, pk=pk)
     return render(request, 'cancionero/cancion_detalle.html', {'cancion': cancion})
 
-
+@login_required
 def subir_medio_view(request, cancion_id):
     cancion = get_object_or_404(Cancion, id=cancion_id)
     cancion.subir_medio()
@@ -18,6 +19,7 @@ def subir_medio_view(request, cancion_id):
 
     return render(request, 'cancionero/cancion_detalle.html', {'cancion': cancion})
 
+@login_required
 def bajar_medio_view(request, cancion_id):
     cancion = get_object_or_404(Cancion, id=cancion_id)
     cancion.bajar_medio()
